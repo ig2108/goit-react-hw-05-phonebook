cb3806b1172c9abe55d18570ab3a9aadd4e9683c
Ivan Golubey <ig2108@yahoo.com>
2020-04-09 16:24:00 +0300

    hw-05 update #1

---
+++ build/precache-manifest.2a7485db9d7e976cfa3a54c7693a9d3f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e46703d34e9e6b3566e95681f0c2c5ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dab614adc341326a3b7",
+    "url": "/static/css/2.6f44476b.chunk.css"
+  },
+  {
+    "revision": "4ebd3381e767f2a19cb3",
+    "url": "/static/css/main.6d485d6d.chunk.css"
+  },
+  {
+    "revision": "1dab614adc341326a3b7",
+    "url": "/static/js/2.faed0f9a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.faed0f9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ebd3381e767f2a19cb3",
+    "url": "/static/js/main.bf8946a1.chunk.js"
+  },
+  {
+    "revision": "12e76917c232aa46c653",
+    "url": "/static/js/runtime-main.07f572ce.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
